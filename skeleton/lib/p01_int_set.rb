9731fae22cfc77f1029d54b712c22e1f5cc94202
Trevor OConnor <trevoroc22@gmail.com>
2017-04-07 11:40:06 -0700

    ResizingIntSet

--- skeleton/lib/p01_int_set.rb
+++ skeleton/lib/p01_int_set.rb
@@ -44,7 +44,7 @@ class IntSet
   end
 
   def remove(num)
-    self[num].each.with_index { |el, i| self[num].delete_at(i) if num == el }
+    self[num].delete(num)
   end
 
   def include?(num)
@@ -73,18 +73,30 @@ class ResizingIntSet
   end
 
   def insert(num)
+    unless include?(num)
+      self[num] << num
+      @count += 1
+      resize! if @count >= num_buckets
+    end
   end
 
+  #
   def remove(num)
+    if include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
-
+  #
   def include?(num)
+    self[num].each { |el| return true if num == el }
+    false
   end
 
   private
-
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -92,5 +104,11 @@ class ResizingIntSet
   end
 
   def resize!
+    old_store = @store
+    @count = 0
+    @store = Array.new(num_buckets * 2) {Array.new}
+    old_store.flatten.each {|el| insert(el) }
   end
+
+  
 end
