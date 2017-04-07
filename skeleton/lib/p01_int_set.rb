17278362ce5cc7d0ce603b39ec24652b8a9dfd65
Trevor OConnor <trevoroc22@gmail.com>
2017-04-07 11:12:02 -0700

    Implement MaxIntSet

---
+++ skeleton/lib/p01_int_set.rb
@@ -0,0 +1,93 @@
+class MaxIntSet
+  def initialize(max)
+    @store = Array.new(max)
+  end
+
+  def insert(num)
+    helper(num) { |num| @store[num] = true }
+  end
+
+  def remove(num)
+    helper(num) { |num| @store[num] = false }
+  end
+
+  def include?(num)
+    helper(num) { |num| @store[num] }
+  end
+
+  private
+
+  def helper(num, &prc)
+    if num.between?(0, @store.length-1)
+      prc.call(num)
+    else
+      raise "Out of bounds"
+    end
+  end
+
+  # def is_valid?(num)
+  #
+  # end
+  #
+  # def validate!(num)
+  # end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    @store[num % @store.length] << num
+  end
+
+  def remove(num)
+    
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+  end
+end
