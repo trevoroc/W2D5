a8bdc6c0d163427a81599ccfafac89a7327d3f0f
Trevor OConnor <trevoroc22@gmail.com>
2017-04-07 15:49:07 -0700

    Implement LRUCache

--- skeleton/lib/p06_lru_cache.rb
+++ skeleton/lib/p06_lru_cache.rb
@@ -15,6 +15,12 @@ class LRUCache
   end
 
   def get(key)
+    if @map.include?(key)
+      update_link!(@map[key])
+    else
+      calc!(key)
+    end
+    @store.last.val
   end
 
   def to_s
@@ -25,12 +31,20 @@ class LRUCache
 
   def calc!(key)
     # suggested helper method; insert an (un-cached) key
+    val = @prc.call(key)
+    @store.append(key, val)
+    @map[key] = @store.last
+    eject! if count > @max
   end
 
   def update_link!(link)
     # suggested helper method; move a link to the end of the list
+    link.remove
+    @store.append(link.key, link.val)
   end
 
   def eject!
+    @map.delete(@store.first.key)
+    @store.first.remove
   end
 end
